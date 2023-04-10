d2f84c1854911ee3d34d35f8008157b59ef2c944
Charlie <charliemanaog@gmail.com>
2023-04-10 17:14:06 +0200

    Checkpoint #2
    - Procedura BuildArray implementata
    - Procedura DoubleIt implementata
    - Programmino funziona correttamente (testato su QtSpim)

--- main.asm
+++ main.asm
@@ -8,6 +8,11 @@ str:	.asciiz ";\n"
 main:	lw $a0, dim1
 		la $a1, array1
 		jal PrintArray
+		lw $a0, dim1
+		jal BuildArray
+		move $a0, $s1
+		la $a1, array2
+		jal PrintArray
 		li $v0, 10
 		syscall
 
@@ -16,7 +21,8 @@ PrintArray:
 		move $t1, $a1
 		move $t2, $zero
 	
-ciclo:	lw $a0, 0($t1)
+PrintCycle:	
+		lw $a0, 0($t1)
 		li $v0, 1
 		syscall
 		li $v0, 4
@@ -24,5 +30,30 @@ ciclo:	lw $a0, 0($t1)
 		syscall
 		addi $t2, $t2, 1
 		addi $t1, $t1, 4
-		blt $t2, $t0, ciclo
+		blt $t2, $t0, PrintCycle
+		jr $ra
+
+BuildArray:
+		move $t0, $a0
+		move $t1, $a1
+		move $t2, $zero
+		la $t3, array2
+		sw $ra, 0($sp)
+
+BuildCycle:
+		lw $a0, 0($t1)
+		sw $a0, 0($t3)
+		jal DoubleIt
+		addi $t3, $t3, 4
+		sw $s0, 0($t3)
+		addi $t1, $t1, 4
+		addi $t2, $t2, 1
+		addi $t3, $t3, 4
+		blt $t2, $t0, BuildCycle
+		lw $ra, 0($sp)
+		add $s1, $t0, $t0
+		jr $ra
+
+DoubleIt:
+		add $s0, $a0, $a0
 		jr $ra
